fd8a7d71b010c569094d371b26dd6902b2e2bcf0
nataly gomez <natalygomez0302@gmail.com>
2024-10-30 11:18:09 -0400

    audio

--- main.js
+++ main.js
@@ -15,6 +15,15 @@ axios.get('https://api.institutoalfa.org/api/songs').then((res) => {
             <p>${song.author}</p>
         </div>`
 
+
+        div.addEventListener("click", () => {
+           document.getElementById("current-song-image").setAttribute("src", `https://api.institutoalfa.org/api/songs/image/${song.image.filename}`)
+           document.getElementById("current-song-title").innerHTML = song.title
+           document.getElementById("current-song-artista").innerHTML = song.author
+
+           document.getElementById("audio").setAttribute("src", `https://api.institutoalfa.org/api/songs/audio/${song.audio.filename}`)
+        })
+
         contenedor.appendChild(div)
     })
 })
